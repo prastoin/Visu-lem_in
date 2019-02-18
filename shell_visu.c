9797e774fc156ef7602eb36e7631b1281b7a5109
Paul RASTOIN <prastoin@e1r7p10.42.fr>
2019-02-18 14:57:45 +0100

    visu shell a fixer

--- shell_visu.c
+++ shell_visu.c
@@ -6,7 +6,7 @@
 /*   By: prastoin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/12 22:07:36 by prastoin          #+#    #+#             */
-/*   Updated: 2019/02/13 06:10:58 by prastoin         ###   ########.fr       */
+/*   Updated: 2019/02/18 10:59:14 by prastoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int		ft_shell_tracertrait(t_data *data, int xstart, int ystart, int x1, int y1, 
 	const int	dx = x1 > xstart ? x1 - xstart : xstart - x1;
 	const int	dy = y1 >ystart ? y1 - ystart : ystart - y1;
 	int			e[2];
-	int			cpt;
 	int			save_x;
 	int			save_y;
 	int			verif;
@@ -30,10 +29,10 @@ int		ft_shell_tracertrait(t_data *data, int xstart, int ystart, int x1, int y1, 
 	save_y = ystart;
 	while (xstart != x1 || ystart != y1)
 	{
-		printf("xstart %d et ystart %d ymax =%d\n", x1, y1, data->x_max * 2);
+		printf("xstart %d et ystart %d xmax =%d ymax = %d\n", x1, y1, data->x_max * 4 + 4, data->y_max * 4);
 		if (ystart >= 0 && ystart < (data->y_max * 4) &&
 				y1 < (data->y_max * 4) && y1 >= 0 && xstart >= 0 &&
-				xstart < (data->x_max * 4) && x1 < (data->x_max * 4) && x1 > 0)
+				xstart <= (data->x_max * 4 + 4) && x1 <= (data->x_max * 4 + 4) && x1 > 0)
 		{
 			printf("je trace\n");
 			if (jeu[ystart][xstart] == ' ')
@@ -63,10 +62,6 @@ int		ft_shell_tracertrait(t_data *data, int xstart, int ystart, int x1, int y1, 
 							if (jeu[ystart + y_inc][xstart] == ' ')
 							jeu[ystart + y_inc][xstart] = '_';
 						}
-//						if (jeu[ystart + 1][xstart] == ' ')
-//						{
-//							jeu[ystart + 1][xstart] = '_';
-//						}
 					}
 					verif = 0;
 				}
@@ -77,12 +72,10 @@ int		ft_shell_tracertrait(t_data *data, int xstart, int ystart, int x1, int y1, 
 		if ((e[1] = e[0]) > -dx)
 		{
 			e[0] -= dy;
-			cpt = -1;
 			xstart += x_inc;
 		}
 		if (e[1] < dy)
 		{
-			cpt = 1;
 			e[0] += dx;
 			ystart += y_inc;
 		}
@@ -110,11 +103,24 @@ void	ft_get_coord_max(t_room *room, t_data *data, int nbroom)
 void	ft_print_dbchar(char **tab)
 {
 	int		i;
+	int		k;
+	int		count;
 
+	count = 0;
+	k = 0;
 	i = 0;
 	while (tab[i])
 	{
-		printf("=%s\n",tab[i]);
+		k = 0;
+		count = 0;
+		while(tab[i][k])
+		{
+			if (tab[i][k] != ' ' && tab[i][k] != '\0')
+				count++;
+			k++;
+		}
+		if (count != 0)
+			printf("%s\n",tab[i]);
 		i++;
 	}
 }
@@ -128,34 +134,26 @@ int			ft_init_shell(t_data *data, t_room *room, t_ant *ant)
 	(void)ant;
 	i = 0;
 	ft_get_coord_max(room, data, data->room);
-	if (!(data->jeu = (char **)malloc(sizeof(char *) * 10000)))
+	if (!(data->jeu = (char **)malloc(sizeof(char *) * (data->y_max * 8 + 1))))
 		return (-1);
-	data->jeu[(data->y_max * 2) + 1] = NULL;
-	while (i < ((data->y_max * 2) + 1))
+	data->jeu[data->y_max * 8] = NULL;
+	while (i < ((data->y_max * 8)))
 	{
-		if (!(data->jeu[i] = (char *)malloc(sizeof(char) * 10000)))
+		if (!(data->jeu[i] = (char *)malloc(sizeof(char) * (data->x_max * 8 + 1))))
 			return (-1);
-		data->jeu[i][data->x_max * 3] = '\0';
+		data->jeu[i][data->x_max * 6] = '\0';
 		j = 0;
 		while (data->jeu[i][j])
 		{
 			data->jeu[i][j] = ' ';
 			j++;
 		}
-		while (j < data->x_max * 5)
+		while (j < data->x_max * 8)
 		{
 			data->jeu[i][j] = '\0';
 			j++;
 		}
 		i++;
-	}
-	if (!(data->len = (int *)malloc(sizeof(int) * (data->y_max + 1))))
-		return (-1);
-	j = 0;
-	while (j <= data->y_max)
-	{
-		data->len[j] = 0;
-		j++;
 	}
 	return (0);
 }
@@ -167,12 +165,12 @@ void		ft_place_room(char **jeu, t_room *room, t_data *data, int curr_room)
 	int	i;
 	int	k;
 
-	k = 0;
+	k = 1;
 	(void)data;
 	i = 0;
-	y = room[curr_room].y * 2;
-	x = room[curr_room].x * 2;
-	while (jeu[y][x + k] != ' ')
+	y = room[curr_room].y * 4;
+	x = room[curr_room].x * 4;
+	while (jeu[y][x + k] && jeu[y][x + k] != ' ')
 		k++;
 	if (k != 0)
 		k++;
@@ -184,106 +182,20 @@ void		ft_place_room(char **jeu, t_room *room, t_data *data, int curr_room)
 		i++;
 	}
 	jeu[y][x + i + 1] = ']';
-	if (i > data->x_max)
-		jeu[y][x + i * 2] = '\0';
-}
-
-int		ft_check_coord(int x, int y, t_room *room, int nbroom)
-{
-	int		i;
-
-	i = 0;
-	while (i < nbroom)
-	{
-		if (room[i].x  == x && room[i].y == y)
-			return (i);
-		i++;
-	}
-	return (-1);
+//	if (i > data->x_max)
+//		jeu[y][x + i * 2] = '\0';
 }
 
 void		ft_fill_jeu(char **jeu, t_room *room, t_data *data)
 {
-	int		x;
 	int		y;
-	int		index;
 
-	index = 0;
 	y = 0;
-	while (jeu[y])
+	while (y < data->room)
 	{
-		x = 0;
-		while (x < (data->x_max + 1))
-		{
-			index = ft_check_coord(x, y, room, data->room);
-			if (index != -1)
-				ft_place_room(jeu, room, data, index);
-			x++;
-		}
+		ft_place_room(jeu, room, data, y);
 		y++;
 	}
-}
-
-int		ft_check_corner(char **jeu, int x, int y, t_data *data)
-{
-	printf("%d et %d\n", x, y);
-	if (y - 1 >= 0 && x - 1 >= 0 && y + 1 < data->y_max * 2 + 1)
-	{
-		if (jeu[y - 1][x - 1] != '/' && jeu[y + 1][x + 1] != '/'
-				&& jeu[y + 1][x - 1] != '\\' && jeu[y - 1][x + 1] != '\\')
-		return (0);
-	}
-	return (-1);
-}
-
-void		ft_place_links(char **jeu, t_room *room, t_data *data, int indexa, int indexb)
-{
-	int		x1;
-	int		y1;
-	int		x;
-	int		y;
-	int		cpt;
-
-	cpt = 0;
-	(void)data;
-	x1 = room[indexb].x * 2 + 1;
-	x = room[indexa].x * 2 + 1;
-	y1 = room[indexb].y * 2;
-	y = room[indexa].y * 2;
-	printf("x1 %d y1 %d\nx  %d y  %d\n", x1, y1, x, y);
-	while (y != y1 && jeu[y])
-	{
-		if (jeu[y][x] == ' ')
-		{
-			if ((y == y1 - 1 || y == y1 + 1 ) && x < x1)
-				jeu[y][x] = '/';
-			else if ((y == y1 - 1 || y == y1 + 1 ) && x > x1)
-				jeu[y][x] = '\\';
-			else
-				jeu[y][x] = '|';
-			cpt++;
-		}
-		if (y > y1)
-			y--;
-		else
-			y++;
-	}
-	while (x != x1 && jeu[y][x])
-	{
-		if (jeu[y][x] == ' ')
-			jeu[y][x] = cpt == 1 ? '-' : '_';
-			if (y == y1 - 1 || y == y1 + 1 || x == x1 - 1 || x == x1 + 1)
-			if (ft_check_corner(jeu, x, y, data) == 0 && jeu[y][x] == '-')
-				jeu[y][x] = ' ';
-		if (x >= x1)
-		{
-			cpt = 1;
-			x--;
-		}
-		else
-			x++;
-	}
-	ft_print_dbchar(jeu);
 }
 
 int		ft_get_links(t_room *room, t_data *data)
@@ -297,16 +209,17 @@ int		ft_get_links(t_room *room, t_data *data)
 		data->step1++;
 	i = data->step1;
 	printf("%s\n", data->map[data->step1]);
-	while (data->map[i][0] != 'L' && data->map[i])
+	while (data->map[i] && data->map[i][0] != 'L')
 	{
 		if (data->map[i][0] != '#')
 		{
 			len = ft_len_to_c(data->map[i], '-');
 			indexa = ft_index_for(ft_strndup(data->map[i], len), room, data->room);
 			indexb = ft_index_for(ft_strdup(data->map[i] + len + 1), room, data->room);
-			printf("indexa %s indexb %s\n", room[indexa].name , room[indexb].name);
 			if (indexa != -1 && indexb != -1)
-				ft_shell_tracertrait(data, room[indexa].x * 2 + 1, room[indexa].y * 2, room[indexb].x * 2 + 1, room[indexb].y * 2, data->jeu);
+				printf("indexa %s indexb %s\n", room[indexa].name , room[indexb].name);
+			if (indexa != -1 && indexb != -1)
+				ft_shell_tracertrait(data, ((room[indexa].x * 4) + 4), (room[indexa].y * 4), ((room[indexb].x * 4) + 4), (room[indexb].y * 4), data->jeu);
 		}
 		i++;
 	}
@@ -315,14 +228,10 @@ int		ft_get_links(t_room *room, t_data *data)
 
 int		ft_shell_visu(t_data *data, t_room *room, t_ant *ant)
 {
-	//	ft_print_dbchar(data->map);
 	if ((ft_init_shell(data, room, ant)) == -1)
 		return (-1);
-	//	ft_print_dbchar(data->jeu);
 	ft_fill_jeu(data->jeu, room, data);
 	ft_get_links(room, data);
-	//	ft_place_links(data->jeu, room, data);
-	//	ft_print_struc(room, data->room);
 	ft_print_dbchar(data->jeu);
 	return (0);
 }
